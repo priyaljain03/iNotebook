7a6ccd0aca2f96112b9926c0e2f8f0c09f6f8672
priyaljain03 <jainpriyal03@gmail.com>
2022-01-02 03:42:17 +0530

    Added Login User

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -27,10 +27,8 @@ router.post('/createuser', [
             return res.status(400).json({ error: "Sorry this user already exits" })
         }
 
-        
         const salt = await bcrypt.genSalt(10);
-        const secPass = await bcrypt.hash(req.body.password,salt)
-        
+        const secPass = await bcrypt.hash(req.body.password, salt)
 
         user = await User.create({
             name: req.body.name,
@@ -40,11 +38,11 @@ router.post('/createuser', [
 
         const data = {
             user: {
-                id:user.id
+                id: user.id
             }
         }
-        const authToken = jwt.sign(data,JWT_SECRET)
-        res.json({authToken})
+        const authToken = jwt.sign(data, JWT_SECRET)
+        res.json({ authToken })
     } catch (error) {
         console.log(error.message)
         res.status(500).send("Some error occurred")
@@ -53,6 +51,41 @@ router.post('/createuser', [
     //   then(user => res.json(user)).catch((error)=>console.log(error),res.json({error:"Please enter unique value for email"}))
 })
 
+//Authenticate a User using : POST "/api/auth/login". No login required
+router.post('/login', [
+    body('email', 'Enter a Valid Email').isEmail(),
+    body('password', 'Password cannot be blank').exists()
+], async (req, res) => {
+    //If there are errors return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
 
+    const {email,password} = req.body
+    try {
+        let user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({error:"Incorrect Email or Password"})
+        }
 
+        const passwordCompare = await bcrypt.compare(password,user.password)
+        console.log(passwordCompare)
+        if(!passwordCompare){
+            return res.status(400).json({error:"Incorrect Email or Password"})
+        }
+
+        const payload = {
+            user:{
+                id:user.id
+            }
+        }
+
+        const authToken = jwt.sign(payload, JWT_SECRET)
+        res.json({ authToken })
+    }catch(err){
+        console.log(err.message)
+        res.status(500).send("Internal Server error occurred")
+    }
+})
 module.exports = router
