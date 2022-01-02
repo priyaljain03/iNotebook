ce242570424f4d184d1feb908f2b4ad5c2831843
priyaljain03 <jainpriyal03@gmail.com>
2022-01-02 23:01:47 +0530

    Completed Updating of Notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -40,4 +40,40 @@ router.post('/addnote', fetchUser, [
     }
 })
 
+//ROUTER3: Update an existing note /api/auth/updatenote . Login required
+router.put('/updatenote/:id', fetchUser, async(req, res) => {
+    try {
+        const {title,description,tag} = req.body
+        const newNote = {};
+        if(title){
+            newNote.title = title
+        }
+        if(description){
+            newNote.description = description
+        }
+        if(tag){
+            newNote.tag = tag
+        }
+
+        //Find the note to be updated and then update it
+        let note = await Note.findById(req.params.id)
+        console.log(note)
+        if(!note){
+            return res.status(404).send("Not Found")
+        }
+        console.log(note.user)
+        if(note.user.toString() != req.user.id){
+            return res.status(401).send("Access Denied")
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+        res.json({note})
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send("Some error occurred")
+    }
+
+})
+
 module.exports = router
